efa318c3cc1da9546c2ad123b447a1d8b941de2f
Anay-Shrivastava <anayshrivastava1999@gmail.com>
2026-04-25 17:55:14 +0530

    learn basic of javaScript

---
+++ Basic_js/masterOfBasics.js
@@ -0,0 +1,97 @@
+//let a = 10;
+// let b = prompt("Enter your age");//34
+// let c = a - b;
+// console.log(c); //ans 1034 kyunki 34 as a string le rha hai to usko string ke sath concatenate kar dega type of String hoga
+
+//but agar number me change krna h to
+// let age = Number(prompt("enter your age")) //78
+// let d = a - age;
+// console.log(d);
+// console.log(typeof d); // type of Number ayega
+
+
+
+// Number("43") // 43 as a number
+// Number("34a") // NaN not a number
+
+//Swaping the numbers
+
+// let a = 5;
+// let b = 10;
+// let c = a; //c me a ki value store krdi
+// a = b; // a me b ki value store krdi
+// b = c // aur fir b  me c ki value dal di
+
+// console.log(a);
+// console.log(b);
+// a = a + b;
+// b = a - b;
+// a = a - b
+
+// console.log(a);
+// console.log(b);
+
+//swaping variable with destructhring
+// [a, b] = [b, a]
+// console.log(a, b);
+
+
+
+//Operators in JS
+
+// let a = 10;
+// let b = 7;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(Math.floor(a / b));// math.floor se decimal ka number remove krke dega means agar num. 11.55 h to 11 dega
+// console.log(a % b);
+// console.log(a ** b);
+
+
+//++a preIncrements, a++postIncrements
+//let a = 10;
+//a++  //first time use karenge to 10 hi ayega but age fir agar next titme fir use karenge to 11 a jayega kyunki a++ me pahle value add karta hai
+//console.log(a);
+
+// ++a // isme value phle aad hogi fir print hogi to aap phle bhi use krteh to 11 hi aata h but isme 12 ayenge
+
+//  same for --a aur a-- me hoga
+
+
+//Function**************************
+// console.log(Math.round(10.6)); //11 hoga
+// console.log(Math.ceil(10.1));// point me kuch bhi likh do to ak point uper lekar jayega 11
+// console.log(Math.floor(10.8));//10 hoga
+// console.log(Math.trunc(18.98)); //remove the decimal part //18 ans hoga
+// console.log(Math.pow(2, 5));// 2^5 yani 2*2*2*2*2 hoga 32 ans hoga
+// console.log(Math.sqrt(144));// 12 hoga squreroot check krta hai
+// console.log(Math.cbrt());
+// console.log(Math.abs(-15));//negative to positive me convert krta h 15 hoga ans
+// console.log(Math.max(23, 34, 45));// 45 hoga max value return hogi
+// console.log(Math.min(45, 56, 32)); // 32 ans min value return hogi
+// console.log(Math.random());
+// console.log(Math.toFixed());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
