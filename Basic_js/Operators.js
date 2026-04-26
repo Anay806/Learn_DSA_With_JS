d423a3d84fd515c50e48e107c6e7256bc6605c3f
Anay-Shrivastava <anayshrivastava1999@gmail.com>
2026-04-26 12:48:39 +0530

    Operators learning

---
+++ Basic_js/Operators.js
@@ -0,0 +1,47 @@
+//ternary operators (?, :)
+
+//condition ? true ka code : false ka code
+
+//12 > 13 ? "Right input" : "Wroung Input" //wroung ayega
+//112 > 13 ? "Right " : "Wroung" //right
+
+
+//Nested ternary operator
+
+//let num = 0;
+//console.log(num > 0 ? "Positive" : num < 0 ? "Negative" : "Zero");
+
+//switch condition
+
+let day = 2;
+
+
+// switch (day) {
+//   case 1: console.log("Monday");
+//     break;
+
+//   case 2: console.log("TuesDay");
+//     break;
+
+//   default: console.log("invalid")
+
+
+// }
+
+// switch (true) {
+//   case 10 > 5: console.log("hello");
+//     break;
+
+//   case 10 < 5: console.log("Hii");
+//     break
+
+//   default: console.log("Invalid")
+
+
+// }
+
+
+
+
+
+
--- Basic_js/conditional.js
+++ Basic_js/conditional.js
@@ -62,30 +62,80 @@
 
 // }
 
-let unit = Number(prompt('Enter Your Total unit'))//700
-let amount = 0;
+// let unit = Number(prompt('Enter Your Total unit'))//700
+// let amount = 0;
 
-if (isNaN(unit)) {
-  console.log("Wroung Input");
+// if (isNaN(unit)) {
+//   console.log("Wroung Input");
+
+// }
+// if (unit > 400) {
+//   amount = (unit - 400) * 13 // suppose 700-300 //3900
+//   unit = 400
+// }//400
+// if (unit > 200 && unit <= 400) {
+//   amount += (unit - 200) * 8 //1600
+//   unit = 200;
+
+// } //200
+// if (unit > 100 && unit <= 200) {
+//   amount += (unit - 100) * 6 // 600
+//   unit = 100;
+// }
+
+// amount += unit * 4
+
+// console.log(amount);
+
+let amount = Number(prompt("Enter a number"));
+
+if (isNaN(amount)) {
+  console.log('Not a Number');
 
 }
-if (unit > 400) {
-  amount = (unit - 400) * 13 // suppose 700-300 //3900
-  unit = 400
-}//400
-if (unit > 200 && unit <= 400) {
-  amount += (unit - 200) * 8 //1600
-  unit = 200;
+if (amount >= 500) {
+  console.log(Math.floor(amount / 500)); //9 notes
+  amount = amount % 500;
 
-} //200
-if (unit > 100 && unit <= 200) {
-  amount += (unit - 100) * 6 // 600
-  unit = 100;
+}
+if (amount >= 200) {
+  console.log(Math.floor(amount / 200));
+  amount = amount % 200;
+
+}
+if (amount >= 100) {
+  console.log(Math.floor(amount / 100));
+  amount = amount % 100
+
+} if (amount >= 50) {
+  console.log(Math.floor(amount / 50));
+  amount = amount % 50
+
+}
+if (amount >= 20) {
+  console.log(Math.floor(amount / 20));
+  amount = amount % 20
+
+} if (amount >= 10) {
+  console.log(Math.floor(amount / 10));
+  amount = amount % 10
+
+}
+if (amount >= 5) {
+  console.log(Math.floor(amount / 5));
+  amount = amount % 5
+
+} if (amount >= 2) {
+  console.log(Math.floor(amount / 2));
+  amount = amount % 2
+
+} if (amount === 1) {
+  console.log(Math.floor(amount / 1));
+
+
 }
 
-amount += unit * 4
 
-console.log(amount);
 
 
 
