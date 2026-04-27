94b16f0a9ae4b23c700b3562862d43c873befe9b
Anay-Shrivastava <anayshrivastava1999@gmail.com>
2026-04-27 16:34:30 +0530

    lerning Loops

---
+++ Basic_js/loops.js
@@ -0,0 +1,41 @@
+//Loops***************
+
+// for loop
+// for (start; end; change) {
+// }
+
+
+// 1-100 tak ka number print karo
+// for (let i = 1; i <= 100; i++) {
+//   console.log(i);
+
+// }
+
+//200 se 100 tak ka loops print karo
+// for (let i = 200; i >= 100; i--) {
+//   console.log(i);
+
+// }
+
+//sum of N natural numbers
+
+// let num = Number(prompt("Enter your number"))
+// let sum = 0;
+
+// for (let i = 0; i <= num; i++) {
+//   sum = sum + i;
+
+// }
+// console.log(sum);
+
+//Q. calculate the faktorial
+let num = Number(prompt("enter your numner"))
+
+let fact = 1;
+for (let i = 1; i <= num; i++) {
+  fact = fact * i
+
+}
+console.log(fact);
+
+
