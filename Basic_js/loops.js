b76f2e4dc331e248590c84b9bc1b149486e8d052
Anay-Shrivastava <anayshrivastava1999@gmail.com>
2026-05-01 16:40:39 +0530

    Leaning pattern logic

--- Basic_js/loops.js
+++ Basic_js/loops.js
@@ -128,27 +128,33 @@
 
 
 // guess the number game***************
-let random = Math.floor(Math.random() * 100) + 1
+// let random = Math.floor(Math.random() * 100) + 1
 
-let guess = -1;
-while (guess !== random) {
-  guess = Number(prompt("Guess the number"))
-  if (isNaN(guess) || guess < 1 || guess > 100) {
-    console.log("Try again between 1-100");
-    continue
+// let guess = -1;
+// while (guess !== random) {
+//   guess = Number(prompt("Guess the number"))
+//   if (isNaN(guess) || guess < 1 || guess > 100) {
+//     console.log("Try again between 1-100");
+//     continue
 
-  }
-  if (guess > random) {
-    console.log("Its too High");
+//   }
+//   if (guess > random) {
+//     console.log("Its too High");
 
-  } else if (guess < random) {
-    console.log("its too Low");
+//   } else if (guess < random) {
+//     console.log("its too Low");
 
-  } else {
-    console.log("Congrats 🎉 and number was: ", guess);
+//   } else {
+//     console.log("Congrats 🎉 and number was: ", guess);
 
-  }
-}
+//   }
+// }
+
+// let display = document.getElementById("CalcId")
+
+// function press(value) {
+//   CalcId.value += value;
+// }
 
 
 
---
+++ Basic_js/pattern.js
@@ -0,0 +1,17 @@
+// process.stdout.write("Hello ")
+// process.stdout.write("world") //ye node enviroment ka func h isme hello world ayega
+
+let prompt = require('prompt-sync')();
+let num = Number(prompt("Enter a number")) //10
+// process.stdout.write(a) //10
+
+for (let i = 1; i <= num; i++) {
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write("1 ",)
+  }
+
+  console.log();
+
+
+}
+
