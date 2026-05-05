1c8d3664110ebaa007d8ed7a3cac4067ccb2dfda
Anay-Shrivastava <anayshrivastava1999@gmail.com>
2026-05-05 23:17:38 +0530

    Learning Array base of theory and function

---
+++ Array/script.js
@@ -0,0 +1,26 @@
+// let arr = []
+// arr.push(20)
+// arr.push(30)
+// arr.push(40)
+// arr.push(50)
+
+// console.log(arr);
+
+// console.log(arr[1]);
+
+// arr.push("Anay")
+
+// arr.pop()
+
+// let arr = new Array(3)
+// arr[0] = 10;
+// arr[1] = 20;
+// arr[2] = 30;
+// arr[3] = 40;
+
+// arr[10] = 150; // arr ke 10th value me 150 store ho jayega
+
+// console.log(arr);
+
+let arr = [10, 20, 30, 40, "Anay", "Shri"]
+
