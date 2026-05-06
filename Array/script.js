02b1d640e4318d458983825b457c37ace9dd2ee6
Anay-Shrivastava <anayshrivastava1999@gmail.com>
2026-05-06 23:46:07 +0530

    Learning Array-01 class

--- Array/script.js
+++ Array/script.js
@@ -1,4 +1,5 @@
-// let arr = []
+//Array is linier data structure and  Array is the collection of value, means ak se jyada value aap sakh sakte ho means number, string, ya array ke array bhi rakh sakte ho
+//let arr = []
 // arr.push(20)
 // arr.push(30)
 // arr.push(40)
@@ -12,7 +13,7 @@
 
 // arr.pop()
 
-// let arr = new Array(3)
+// let arr = new Array(3)  yaha par nya array banaya aur uska size 3 de diya par js me  size ka limit nhi h
 // arr[0] = 10;
 // arr[1] = 20;
 // arr[2] = 30;
@@ -22,5 +23,21 @@
 
 // console.log(arr);
 
-let arr = [10, 20, 30, 40, "Anay", "Shri"]
+//let arr = [10, 20, 30, 40, "Anay", "Shri"]
+
+// let arr = new Array(3)
+// arr.push(10);
+// console.log(arr); // yaha par arr ki value  starting ka 3 value chodkar  4 value par ayegi
+
+//prompt se value lena aur array me put karna
+
+let arr = new Array(5);
+
+for (let i = 0; i < arr.length; i++) {
+  arr[i] = Number(prompt("Enter a value"))
+}
+console.log(arr);
+
+
+
 
