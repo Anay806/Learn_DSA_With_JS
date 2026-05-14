9d26e4c598f966e0ea00e2d7727faa6f6cc766fb
Anay-Shrivastava <anayshrivastava1999@gmail.com>
2026-05-14 17:19:13 +0530

    learning new question of Array

--- Array/script.js
+++ Array/script.js
@@ -137,6 +137,21 @@
 // console.log(min);
 
 
+//reverse array
+
+// let arr = [1, 2, 3, 4, 5, 6]
+
+// let temp = new Array(arr.length)
+// let j = 0
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   temp[j] = arr[i]
+//   j++
+
+// }
+// console.log(temp);
+
+
+
 
 
 
